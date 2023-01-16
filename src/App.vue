2710ecf3e94f539146ead0584110178c1d6f5d10
Anwarul Islam <anwaarulislaam@gmail.com>
2023-01-16 21:07:25 +0600

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header">
-    <h1>Testing Apollo and Urql</h1>
+    <h1>Testing {{ client === "apollo" ? "Apollo" : "URQL" }} Client</h1>
     <button @click="changeClient">
       {{ client === "apollo" ? "Switch to Urql" : "Switch to Apollo" }}
     </button>
@@ -8,14 +8,14 @@
 
   <!-- toggle to switch to apollo or urql -->
 
-  <ApolloClient v-if="client === 'apollo'" />
-  <UrqlClient v-else />
+  <ApolloClientContent v-if="client === 'apollo'" />
+  <UrqlClientContent v-else />
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-import ApolloClient from "./components/Apollo.vue";
-import UrqlClient from "./components/Urql.vue";
+import ApolloClientContent from "./components/Apollo.vue";
+import UrqlClientContent from "./components/Urql.vue";
 
 const client = ref("apollo");
 
--- src/components/Urql.vue
+++ src/components/Urql.vue
@@ -1,3 +1,53 @@
 <template>
-  <h1>urql Client</h1>
+  <div class="header">
+    <h2>URQL Books</h2>
+    <button @click="fetchBooks">
+      {{ isLoading ? "Loading..." : "Fetch Books" }}
+    </button>
+  </div>
+
+  <div>
+    <ul>
+      <li v-for="book in books" :key="book.id">
+        {{ book.name }}
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const books: any = ref([]);
+const isLoading = ref(false);
+
+const fetchBooks = async () => {
+  isLoading.value = true;
+
+  // wait 1 second
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const response = await fetch("http://localhost:4000/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+          books {
+            id
+            name
+          }
+        }
+      `,
+    }),
+  });
+
+  const { data } = await response.json();
+
+  isLoading.value = false;
+
+  books.value = data.books;
+};
+</script>
